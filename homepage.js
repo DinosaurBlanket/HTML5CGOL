68f3360813040e16610fbd07c8e6680093f6c3dd
DinosaurBlanket <Jon.Arintok@gmail.com>
2013-06-19 22:13:15 -0400

    draw with the cursor

--- homepage.js
+++ homepage.js
@@ -4,22 +4,54 @@ var c0 = document.getElementById("canvas0").getContext("2d");
 
 var width = 800;
 var height = 600;
-var cellsize = 8;
-var timeout = 40;
+var cellsize = 4;
+var timeout = 80;
 var generation = 0;
-var offcolor = "#CCC", oncolor = "#222";
+var offcolor = "#DDD", oncolor = "#444";
 var pitch = width/cellsize;
 
-var cells = [], neighborcounts = [], arraylength = pitch*(height/cellsize);
+var cells = [], cursedcells = [], neighborcounts = [];
+var arraylength = pitch*(height/cellsize);
 for (var i=0; i<arraylength; i++) {
 	cells[i] = 0;
 	neighborcounts[i] = 0;
 }
 
+var curx, cury, mousedown = 0;
+var curse = function(evt) {
+	var obj = canvas0;
+	var top = 0;
+	var left = 0;
+	while (obj && obj.tagName != "BODY") {
+		top += obj.offsetTop;
+		left += obj.offsetLeft;
+		obj = obj.offsetParent;
+	}
+	curx = evt.clientX - left + window.pageXOffset;
+	cury = evt.clientY - top + window.pageYOffset;
+	if (mousedown) cursedcells.push( Math.floor(cury/cellsize)*pitch + Math.floor(curx/cellsize) );
+}
+canvas0.addEventListener("mousemove", curse, 0)
+canvas0.addEventListener("mousedown", function(evt) {
+	mousedown = 1;
+	curse(evt);
+}, 0);
+canvas0.addEventListener("mouseup",   function(evt) {mousedown = 0}, 0);
+
 function fillcell(i) { c0.fillRect( (i%pitch)*cellsize, Math.floor(i/pitch)*cellsize, cellsize, cellsize ) }
 
 function loop() {
 	//console.log("generation : " + generation);
+	
+	while (cursedcells.length) {
+		var cc = cursedcells.pop()
+		cells[cc-pitch  -1] = 1;
+		cells[cc-pitch  -2] = 1;
+		cells[cc-pitch*2-1] = 1;
+		cells[cc-pitch*2-3] = 1;
+		cells[cc-pitch*3-2] = 1;
+		cells[cc-pitch*3-3] = 1;
+	}
 	//to what cells does each live cell neighbor?
 	for (var i=0; i<arraylength; i++) {
 		if (cells[i]) {
@@ -58,16 +90,9 @@ function loop() {
 	//reset
 	for (var i=0; i<arraylength; i++) neighborcounts[i] = 0;
 	generation++;
+	
 	window.setTimeout(loop, timeout);
-}
-
-
-function blitpattern(x, y, source) {
-	for (var i=0; i<source.length; i++) {
-		for (var j=0; j<source[i].length; j++) {
-			cells[ (y+i)*pitch + x + source[i][j] ] = 1;
-		}
-	}
+	
 }
 
 gosperglidergun = [
@@ -82,9 +107,17 @@ gosperglidergun = [
 	[12, 13] 
 ]
 
+function blitpattern(x, y, source) {
+	for (var i=0; i<source.length; i++) {
+		for (var j=0; j<source[i].length; j++) {
+			cells[ (y+i)*pitch + x + source[i][j] ] = 1;
+		}
+	}
+}
 
 blitpattern(20, 20, gosperglidergun);
 
 loop();
+
 
 }//init
